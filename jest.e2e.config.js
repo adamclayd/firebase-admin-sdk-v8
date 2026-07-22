638da2baa89f0742e28bc7d26c3b5db283daab73
Adam <adamclayd@gmail.com>
2026-07-22 17:13:40 -0500

    cleaned up repo on npm

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -4,7 +4,7 @@ module.exports = {
   testMatch: ['**/*.e2e.ts'],
   testTimeout: 30000, // 30 seconds for real API calls
   roots: ['<rootDir>/src'],
-  globalSetup: '<rootDir>/jest.e2e.setup.ts',
+  globalSetup: '<rootDir>/jest.e2e.setup.js',
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/*.spec.ts',
